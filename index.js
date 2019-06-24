198de49e35ac15e6609a4faa8a9bbddbee1166f6
nidhitripathi23 <ntripathi@isystango.com>
2019-06-24 13:24:24 +0530

    Added gitignore and updated package json

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = new express();
-
+app.use(bodyParser.jsom());
 app.use('/', function(){
     console.log('Date', new Date());
 });
