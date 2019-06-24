9c2313b7dfdf9f5e84a47df2e71068d983acac3e
nidhitripathi23 <ntripathi@isystango.com>
2019-06-24 13:27:09 +0530

    ##JIR:203:Added people post API

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ app.use('/', function(){
 
 app.get('/people', function(req, res){
     console.log('Date', new Date());
-    res.send("hello world!!")
+    res.send("hello world!!");
+});
+
+app.post('/people', function(req, res){
+    console.log('Date', req.body);
+    res.send("hello world!!");
 });
 
 app.listen(3000);
